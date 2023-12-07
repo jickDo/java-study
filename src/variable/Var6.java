15e4f1c9386cc0b93ae665ef3bbce2db873c8d53
jickDo <pocard0117@naver.com>
2023-12-07 20:59:16 +0900

    선언 후 초기화를 하지 않을 때 에러발생

---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a);
+//        컴파일에러 선언후 초기화를 하지 않아서
+    }
+}
