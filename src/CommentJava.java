fb7f04b942138caf7c81f8e0f0fa556a1f15b4b6
jickDo <pocard0117@naver.com>
2023-12-07 14:33:58 +0900

    한줄 주석과 여러줄 주석

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java"); //hello java를 출력합니다
+// 한줄주석       System.out.println("hello java2");
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
