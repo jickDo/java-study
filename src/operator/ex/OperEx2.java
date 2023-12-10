6dbfd08f0479ea54b0f54660a3d53a1af8f32b03
jickDo <pocard0117@naver.com>
2023-12-10 20:18:38 +0900

    연산자 2번째 문제 구현

---
+++ src/operator/ex/OperEx2.java
@@ -0,0 +1,19 @@
+package operator.ex;
+
+public class OperEx2 {
+
+    public static void main(String[] args) {
+
+
+        double num1 = 10;
+        double num2 = 20;
+        double num3 = 30;
+
+        double sum = num1 + num2 + num3;
+        double average = sum/3;
+
+        System.out.println(sum);
+        System.out.println(average);
+
+    }
+}
