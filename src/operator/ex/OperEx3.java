90b92aa451337068dbc2e620ff50a1cb8fd7586c
jickDo <pocard0117@naver.com>
2023-12-10 20:20:59 +0900

    연산자 3번째 예제 구현

---
+++ src/operator/ex/OperEx3.java
@@ -0,0 +1,11 @@
+package operator.ex;
+
+public class OperEx3 {
+
+    public static void main(String[] args) {
+        int score = 83;
+
+        boolean result = score>=80 && score <=100;
+        System.out.println(result);
+    }
+}
