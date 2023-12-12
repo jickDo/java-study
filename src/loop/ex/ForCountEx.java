bc65212ddab7cd802be06ed1b2e975e384e42abd
jickDo <pocard0117@naver.com>
2023-12-12 18:33:05 +0900

    count를 for버전으로

---
+++ src/loop/ex/ForCountEx.java
@@ -0,0 +1,10 @@
+package loop.ex;
+
+public class ForCountEx {
+
+    public static void main(String[] args) {
+        for(int start=1; start<11; start++){
+            System.out.println(start);
+        }
+    }
+}
