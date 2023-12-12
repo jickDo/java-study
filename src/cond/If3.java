8ad7ceb2696701bfa2ee4f129624b3279ba26f12
jickDo <pocard0117@naver.com>
2023-12-12 16:23:40 +0900

    if3 예제 if4예제 구현

---
+++ src/cond/If3.java
@@ -0,0 +1,24 @@
+package cond;
+
+public class If3 {
+
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age<=7){
+            System.out.println("미취학입니다");
+        }
+        if (age>=8 && age<=13) {
+            System.out.println("8세이상 13세 이하입니다");
+        }
+        if (age>=14 && age<=16) {
+            System.out.println("14세이상 16세 이하입니다");
+        }
+        if (age>=17 && age<=19) {
+            System.out.println("17세이상 19세 이하입니다");
+        }
+        if(age>=20) {
+            System.out.println("성인입니다");
+        }
+    }
+}
