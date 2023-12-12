709060c444a3e136910c0daeee6d013acd8588de
jickDo <pocard0117@naver.com>
2023-12-12 18:35:56 +0900

    짝수만 출력 while 버전

---
+++ src/loop/ex/WhilePairNumEx.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class WhilePairNumEx {
+
+    public static void main(String[] args) {
+        int num=1;
+        while(num<11){
+            System.out.println(2*num);
+            num++;
+        }
+    }
+}
