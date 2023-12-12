81b87f338280a1abb0a8fe2a7de5792d3d5e9214
jickDo <pocard0117@naver.com>
2023-12-12 16:52:09 +0900

    switch 4 예제 구현 (자바 14 스위치 구문)

---
+++ src/cond/Switch4.java
@@ -0,0 +1,16 @@
+package cond;
+
+public class Switch4 {
+
+    public static void main(String[] args) {
+        int grade = 3;
+
+        int coupon = switch (grade){
+            case 1 -> 1000;
+            case 2 -> 2000;
+            case 3 -> 3000;
+            default -> 500;
+        };
+        System.out.println("받을수 있는 쿠폰 "+ coupon);
+    }
+}
