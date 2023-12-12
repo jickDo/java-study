deecafe3fe5dcd6473c03a10ba886bcea62db28f
jickDo <pocard0117@naver.com>
2023-12-12 18:34:46 +0900

    짝수 출력을 for버전으로

---
+++ src/loop/ex/ForPairNumEx.java
@@ -0,0 +1,10 @@
+package loop.ex;
+
+public class ForPairNumEx {
+
+    public static void main(String[] args) {
+        for(int num=1; num<11; num++){
+            System.out.println(num*2);
+        }
+    }
+}
