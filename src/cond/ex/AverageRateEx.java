72468f3fc6f98fa9c190c33b4aef0a5f499f1257
jickDo <pocard0117@naver.com>
2023-12-12 17:14:34 +0900

    영화 평점에 따른 추천

---
+++ src/cond/ex/AverageRateEx.java
@@ -0,0 +1,18 @@
+package cond.ex;
+
+public class AverageRateEx {
+
+    public static void main(String[] args) {
+        double rating = 9;
+
+        if (rating >=9){
+            System.out.println("어바웃 타임");
+        }
+        if (rating >=8){
+            System.out.println("토이스토리");
+        }
+        if (rating >=7){
+            System.out.println("고질라");
+        }
+    }
+}
